1228a5e7f54659beb297b1e8f3e5946d69b3dbde
AasthaDixit26 <aaasthadixit@gmail.com>
2023-01-02 16:23:31 +0530

    Excited for #6Companies30DaysChallenge

--- Microsoft/ReversePolishNotation.java
+++ Microsoft/ReversePolishNotation.java
@@ -1,10 +1,35 @@
 package Microsoft;
 
+import java.util.Stack;
+
 public class ReversePolishNotation {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	 public static int evalRPN(String[] tokens) {
+	        Stack<Integer> t=new Stack();
+	        for(int i=0;i<tokens.length;i++) {
+	        	if(tokens[i].equals("+" )|| tokens[i].equals("-")|| tokens[i].equals("*")|| tokens[i].equals("/")) {
+	        		int store1=t.pop();
+	        		int store2=t.pop();
+	        		if(tokens[i].equals("+")) {
+	        			t.push(store2+store1);
+	        		}
+	        		if(tokens[i].equals("-")) {
+	        			t.push(store2-store1);
+	        		}
+	        		if(tokens[i].equals("*")) {
+	        			t.push(store2*store1);
+	        		}
+	        		if(tokens[i].equals("/")) {
+	        			t.push(store2/store1);
+	        		}
+	        		
+	        	}
+	        	else {
+	        		int number=Integer.parseInt(tokens[i]);
+	        		t.push(number);
+	        	}
+	        }
+	    return t.peek();
+	 }
+	
 
 }
